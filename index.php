c1e441048fa930a83d92d18301f4747110d2af7b
Leonardo_Pires <bressanpires8@gmail.com>
2025-04-28 14:10:39 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
-<?php
+<?php 
+
+#iniciar sessão
+session_start();
+
+#Base de dados
+include 'db.php';
+
+#Cabeçalho
 include 'header.php';
 
-$pagina = $_GET['aaa'];
 
-switch($pagina){
-    case 'home':include 'views/home.php'; break;
-    case 'contatos': include 'views/contacts.php'; break;
-    case 'login': include 'views/login.php'; break; 
-    default: include 'views/home.php'; break;
+#Conteúdo da página
+
+if(isset($_SESSION['login'])){//se existir um login
+	if(isset($_GET['pagina'])){
+		$pagina = $_GET['pagina'];
+	}
+	else{
+		$pagina = 'teste';
+	}
+}
+
+else{
+		$pagina = 'home';
+}
+
+switch ($pagina) {
+	case 'teste': include 'views/teste.php'; break;
+	case 'teste2': include 'views/teste2.php'; break;
+	default: include 'views/home.php'; 
+	break;
 }
 
 
+#Rodapé
 include 'footer.php';
